140ebf3044ec813402c0c9b79189e584c68f515a
Federico T <ftedinur@itba.edu.ar>
2015-02-08 05:30:06 -0300

    improved XPath selection path, remove ul subelements from selected nodes

--- wikiquote.py
+++ wikiquote.py
@@ -9,7 +9,7 @@ class DisambiguationPageException(Exception):
 SEARCH_URL = 'http://en.wikiquote.org/w/api.php?format=json&action=query&list=search&continue=&srsearch='
 PAGE_URL = 'http://en.wikiquote.org/w/api.php?format=json&action=parse&prop=text|categories&page='
 MIN_QUOTE_LEN = 6
-MIN_QUOTE_WORDS = 4
+MIN_QUOTE_WORDS = 3
 DEFAULT_MAX_QUOTES = 20
 WORD_BLACKLIST = ['quoted', 'Variant:', 'Retrieved', 'Notes:']
 
@@ -31,7 +31,7 @@ def search(s):
 def is_disambiguation(categories):
 	if not categories:
 		return True
-	
+
 	for cat in categories:
 		if cat['*'] == 'Disambiguation_pages':
 			return True
@@ -53,13 +53,18 @@ def is_quote(txt):
 		return False
 
 	return True
-	
+
 def extract_quotes(html_content, max_quotes):
 	tree = lxml.html.fromstring(html_content)
 	quotes_list = []
-	
-	txt_list = tree.xpath('//ul/li')
+
+	txt_list = tree.xpath('//div/ul/li')
 	for txt in txt_list:
+
+		uls = txt.xpath('ul')
+		for ul in uls:
+			ul.getparent().remove(ul)
+
 		txt = txt.text_content().strip()
 		if is_quote(txt):
 			txt_normal = ' '.join(txt.split())
@@ -69,7 +74,7 @@ def extract_quotes(html_content, max_quotes):
 				break
 
 	return quotes_list
-	
+
 def quotes(page_title, max_quotes = DEFAULT_MAX_QUOTES):
 	data = json_from_url(PAGE_URL + urllib.parse.quote(page_title))
 	if 'error' in data:
