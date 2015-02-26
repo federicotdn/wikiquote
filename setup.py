b65926b9ad3b900eeb8f4fec5b9d2c3e0bb577ee
Federico T <ftedinur@itba.edu.ar>
2015-02-26 01:07:47 -0300

    add setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'wikiquote',
+  py_modules = ['wikiquote'],
+  version = '0.1',
+  description = 'Retrieve quotes from any Wikiquote page.',
+  author = 'Federico Tedin',
+  author_email = 'federicotedin@gmail.com',
+  url = 'https://github.com/federicotdn/python-wikiquotes',
+  download_url = 'https://github.com/federicotdn/python-wikiquotes/tarball/0.1',
+  keywords = ['quotes', 'wikiquote', 'python', 'api', 'qotd'],
+  license = 'MIT',
+  classifiers = [],
+)
