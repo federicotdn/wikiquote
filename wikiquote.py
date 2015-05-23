55fa08fd99de8a6f3cbf88dd909ab81c16a852a1
Denis THULIN <denis.thulin@gmail.com>
2015-05-23 15:16:29 +0200

    Added support for french in wikiquote.quote

--- wikiquote.py
+++ wikiquote.py
@@ -2,6 +2,7 @@ import urllib.request
 import urllib.parse
 import json
 import lxml.html
+from bs4 import BeautifulSoup
 
 
 class NoSuchPageException(Exception):
@@ -11,10 +12,13 @@ class NoSuchPageException(Exception):
 class DisambiguationPageException(Exception):
     pass
 
-W_URL = 'http://en.wikiquote.org/w/api.php'
+W_URL = 'http://{lang}.wikiquote.org/w/api.php'
 SRCH_URL = W_URL + '?format=json&action=query&list=search&continue=&srsearch='
 PAGE_URL = W_URL + '?format=json&action=parse&prop=text|categories&page='
 MAINPAGE_URL = W_URL + '?format=json&action=parse&page=Main%20Page&prop=text'
+CTGRY_MMBRS_URL = W_URL + (
+    '?action=query&continue=-||&cmcontinue={cmcontinue}'
+    '&list=categorymembers&format=json&cmtitle={page}&cmtype={command}')
 MIN_QUOTE_LEN = 6
 MIN_QUOTE_WORDS = 3
 DEFAULT_MAX_QUOTES = 20
@@ -27,11 +31,12 @@ def json_from_url(url):
     return json.loads(body)
 
 
-def search(s):
+def search(s, lang='en'):
     if not s:
         return []
     search_terms = urllib.parse.quote(s)
-    data = json_from_url(SRCH_URL + search_terms)
+    local_srch_url = SRCH_URL.format(lang=lang)
+    data = json_from_url(local_srch_url + search_terms)
     results = [entry['title'] for entry in data['query']['search']]
     return results
 
@@ -98,8 +103,24 @@ def extract_quotes(html_content, max_quotes):
     return quotes_list
 
 
-def quotes(page_title, max_quotes=DEFAULT_MAX_QUOTES):
-    data = json_from_url(PAGE_URL + urllib.parse.quote(page_title))
+def extract_quotes_fr(html_content, max_quotes):
+    '''Extract quotes from the french wiki
+
+    Keyword arguments:
+    html_content -- the data returned by the wikiquote api
+    max_quote -- max number of quotes to retrieve
+    '''
+    soup = BeautifulSoup(html_content)
+    spans = soup.find_all('span')
+    quotes = [span.text
+              for span in spans
+              if 'class' in span.attrs and 'citation' in span['class']]
+    return quotes
+
+
+def quotes(page_title, max_quotes=DEFAULT_MAX_QUOTES, lang='en'):
+    local_page_url = PAGE_URL.format(lang=lang)
+    data = json_from_url(local_page_url + urllib.parse.quote(page_title))
     if 'error' in data:
         raise NoSuchPageException('No pages matched the title: ' + page_title)
 
@@ -108,11 +129,13 @@ def quotes(page_title, max_quotes=DEFAULT_MAX_QUOTES):
             'Title returned a disambiguation page.')
 
     html_content = data['parse']['text']['*']
+    if lang == 'fr':
+        return extract_quotes_fr(html_content, max_quotes)
     return extract_quotes(html_content, max_quotes)
 
 
 def quote_of_the_day():
-    data = json_from_url(MAINPAGE_URL)
+    data = json_from_url(MAINPAGE_URL.format(lang='en'))
     tree = lxml.html.fromstring(data['parse']['text']['*'])
     tree = tree.get_element_by_id('mf-qotd')
 
@@ -120,3 +143,41 @@ def quote_of_the_day():
     quote = raw_quote[0].strip()
     author = raw_quote[1].strip()
     return quote, author
+def quotes_fr_original(page_title, max_quotes=DEFAULT_MAX_QUOTES):
+    '''Extract quotes from the french wiki with original version:
+    Some quotes translated to french come with the original one.
+    this retrive boths as a tuple (french,original).
+    original is None for quote that are french only
+
+    Keyword arguments:
+    html_content -- the data returned by the wikiquote api
+    max_quote -- max number of quotes to retrieve
+    '''
+    local_page_url = PAGE_URL.format(lang='fr')
+    data = json_from_url(local_page_url + urllib.parse.quote(page_title))
+    if 'error' in data:
+        raise NoSuchPageException('No pages matched the title: ' + page_title)
+
+    if is_disambiguation(data['parse']['categories']):
+        raise DisambiguationPageException(
+            'Title returned a disambiguation page.')
+
+    html_content = data['parse']['text']['*']
+    soup = BeautifulSoup(html_content)
+    spans = soup.find_all('span')
+    quotes = [{'span': span, 'vf': span.text}
+              for span in spans
+              if 'class' in span.attrs and 'citation' in span['class']]
+    # get original, if any
+    for quote in quotes:
+        try:
+            quote['vo'] = next(
+                span.text for span in list(
+                    quote['span'].parent.next_siblings)[1].find_all('span')
+                if 'class' in span.attrs and 'original' in span['class'])
+        except IndexError:
+            quote['vo'] = None
+        except StopIteration:
+            quote['vo'] = None
+    quotes = [(quote['vf'], quote['vo']) for quote in quotes]
+    return quotes
