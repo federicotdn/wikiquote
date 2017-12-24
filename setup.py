3e74f69b51b1e1920dde8098ce1abce03710d0d3
Fede <ftedinur@itba.edu.ar>
2017-12-24 17:13:19 -0300

    bump to 0.1.6 (German language)

--- setup.py
+++ setup.py
@@ -3,18 +3,17 @@ from distutils.core import setup
 setup(
   name = 'wikiquote',
   packages = ['wikiquote', 'wikiquote.langs'],
-  version = '0.1.5',
+  version = '0.1.6',
   description = 'Retrieve quotes from any Wikiquote page.',
   author = 'Federico Tedin',
   author_email = 'federicotedin@gmail.com',
   install_requires = ['lxml'],
   url = 'https://github.com/federicotdn/python-wikiquotes',
-  download_url = 'https://github.com/federicotdn/python-wikiquotes/archive/0.1.5.tar.gz',
+  download_url = 'https://github.com/federicotdn/python-wikiquotes/archive/0.1.6.tar.gz',
   keywords = ['quotes', 'wikiquote', 'python', 'api', 'qotd'],
   license = 'MIT',
   classifiers = [
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.4',
     'License :: OSI Approved :: MIT License'
   ],
 )
