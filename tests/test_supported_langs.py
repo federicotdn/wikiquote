7a5e51510574b3e40952f9b3cb55a8f1ef7af0b7
federicotdn <federicotedin.ar@gmail.com>
2018-04-06 09:30:58 -0300

    Added Italian language support.

---
+++ tests/test_supported_langs.py
@@ -0,0 +1,13 @@
+import wikiquote
+import unittest
+
+class SupportedLangsTest(unittest.TestCase):
+    """
+    Test wikiquote.supported_languages()
+    """
+
+    def test_supported_languages(self):
+        # Check that all language modules are being loaded
+        self.assertListEqual(wikiquote.supported_languages(), [
+            'de', 'en', 'es', 'fr', 'it'
+        ])
---
+++ wikiquote/langs/it.py
@@ -0,0 +1,28 @@
+import lxml.etree
+
+from .. import utils
+
+MAIN_PAGE = "Pagina_principale"
+WORD_BLACKLIST = []
+HEADINGS = [
+    'Bibliografia',
+    'Opere',
+    'Altri progetti',
+    'Note',
+    'Voci correlate'
+]
+
+
+def extract_quotes(tree, max_quotes):
+    return utils.extract_quotes_li(tree, max_quotes, HEADINGS, WORD_BLACKLIST)
+
+
+def qotd(html_tree):
+    tree = html_tree.get_element_by_id('mf-Qotd')
+
+    quote_container = tree.xpath('div')[0].text_content().split('„')
+
+    quote = quote_container[0].lstrip('“').strip()
+    author = quote_container[1].strip()
+
+    return quote, author
