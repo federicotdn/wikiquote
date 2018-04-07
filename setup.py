29269033fc375bbf1954a7f612ab2b7ed0dc5637
Federico T <federicotedin@gmail.com>
2018-04-06 23:12:13 -0300

    bump to 0.1.7 (Italian language)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'wikiquote',
     packages = ['wikiquote', 'wikiquote.langs'],
-    version = '0.1.6',
+    version = '0.1.7',
     description = 'Retrieve quotes from any Wikiquote page.',
     author = 'Federico Tedin',
     author_email = 'federicotedin@gmail.com',
@@ -11,11 +11,12 @@ setup(
         'lxml>=4.2, <5.0'
     ],
     url = 'https://github.com/federicotdn/python-wikiquotes',
-    download_url = 'https://github.com/federicotdn/python-wikiquotes/archive/0.1.6.tar.gz',
-    keywords = ['quotes', 'wikiquote', 'python', 'api', 'qotd'],
+    download_url = 'https://github.com/federicotdn/python-wikiquotes/archive/0.1.7.tar.gz',
+    keywords = ['quotes', 'wikiquote', 'python', 'api', 'qotd', 'quote', 'day'],
     license = 'MIT',
     classifiers = [
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License'
-  ],
+        'Programming Language :: Python :: 3 :: Only',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities'
+    ]
 )
