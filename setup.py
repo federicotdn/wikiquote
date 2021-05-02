42192d5118c20242b720cfb47a5559db0c29411e
Federico Tedin <federicotedin@gmail.com>
2021-05-02 22:56:17 +0200

    Reformat setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 from distutils.core import setup
 
-VERSION = '0.1.14'
+VERSION = "0.1.14"
 
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requires = f.read().splitlines()
 
-with open('README.rst') as f:
+with open("README.rst") as f:
     long_description = f.read()
 
 setup(
-    name='wikiquote',
-    packages=['wikiquote', 'wikiquote.langs'],
+    name="wikiquote",
+    packages=["wikiquote", "wikiquote.langs"],
     version=VERSION,
-    description='Retrieve quotes from any Wikiquote article.',
+    description="Retrieve quotes from any Wikiquote article.",
     long_description=long_description,
-    author='Federico Tedin',
-    author_email='federicotedin@gmail.com',
+    author="Federico Tedin",
+    author_email="federicotedin@gmail.com",
     install_requires=requires,
-    url='https://github.com/federicotdn/wikiquote',
-    download_url='https://github.com/federicotdn/wikiquote/archive/{}.tar.gz'.format(VERSION),
-    keywords=['quotes', 'wikiquote', 'python', 'api', 'qotd', 'quote', 'day'],
-    license='MIT',
+    url="https://github.com/federicotdn/wikiquote",
+    download_url="https://github.com/federicotdn/wikiquote/archive/{}.tar.gz".format(
+        VERSION
+    ),
+    keywords=["quotes", "wikiquote", "python", "api", "qotd", "quote", "day"],
+    license="MIT",
     classifiers=[
-        'Programming Language :: Python :: 3 :: Only',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Utilities'
-    ]
+        "Programming Language :: Python :: 3 :: Only",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Utilities",
+    ],
 )
