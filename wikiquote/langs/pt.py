4247f99c403437c4f1a28c92b1ad148d85e7cf11
Lennox <ayibisah@gmail.com>
2024-03-27 16:54:15 +0100

    Refactor: Extract exisiting qotd parser into separate function

--- wikiquote/langs/pt.py
+++ wikiquote/langs/pt.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Text, Tuple
 
 import lxml
@@ -8,6 +9,8 @@ MAIN_PAGE = "Página_principal"
 WORD_BLOCKLIST = ["Fonte"]
 HEADINGS = ["Veja também", "Referências"]
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 def extract_quotes(tree: lxml.html.HtmlElement, max_quotes: int) -> List[Text]:
     # Remove all description elements
@@ -18,7 +21,7 @@ def extract_quotes(tree: lxml.html.HtmlElement, max_quotes: int) -> List[Text]:
     return utils.extract_quotes_li(tree, max_quotes, HEADINGS, WORD_BLOCKLIST)
 
 
-def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
+def qotd_by_element_id(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
     tree = html_tree.get_element_by_id("mf-cdd")
     quote_author = tree.xpath(".//td")[2].text_content().split("-")
 
@@ -26,3 +29,11 @@ def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
     author = quote_author[-1].strip()
 
     return quote, author
+
+
+def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
+    try:
+        return qotd_by_element_id(html_tree)
+    except Exception as e:
+        logger.warning("Could not extract pt QOTD using qotd_by_element_id due to: %s", e)
+    raise utils.MissingQOTDException('Could not extract: All Attempts failed')
