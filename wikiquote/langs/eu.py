080fc8ad93bae9885c6f454aab1883ba33847fd4
Lennox <ayibisah@gmail.com>
2024-03-27 18:20:33 +0100

    Refactor: Extract exisiting qotd parser into separate function

--- wikiquote/langs/eu.py
+++ wikiquote/langs/eu.py
@@ -1,3 +1,4 @@
+import logging
 from typing import List, Text, Tuple
 
 import lxml
@@ -8,13 +9,15 @@ WORD_BLOCKLIST = ["Iturria:", "Jatorrizkoan ", "Testuingurua:"]
 MAIN_PAGE = "Azala"
 HEADINGS = ["kanpo loturak", "erreferentziak"]
 
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 def extract_quotes(tree: lxml.html.HtmlElement, max_quotes: int) -> List[Text]:
     q_lst = utils.extract_quotes_li(tree, max_quotes, HEADINGS, WORD_BLOCKLIST)
     return [utils.remove_credit(q) for q in q_lst]
 
 
-def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
+def qotd_by_element_id(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
     tree = html_tree.get_element_by_id("mf-qotd")
 
     selector = "div/div/table/tbody/tr"
@@ -23,3 +26,12 @@ def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
     author = raw_quote[1].strip()
 
     return quote, author
+
+
+def qotd(html_tree: lxml.html.HtmlElement) -> Tuple[Text, Text]:
+    try:
+        return qotd_by_element_id(html_tree)
+    except Exception as e:
+        logger.warning("Could not extract eu QOTD using qotd_by_element_id due to: %s", e)
+
+    raise utils.MissingQOTDException('Could not extract: All Attempts failed')
